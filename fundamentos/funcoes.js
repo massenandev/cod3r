f42b52246aa23c6b801a4f346eb5d8b23a17c03a
massenandev <massenandev@gmail.com>
2021-05-21 15:13:17 -0300

    about functions

---
+++ fundamentos/funcoes.js
@@ -0,0 +1,8 @@
+//js é totalmente baseado em funções
+//obj em js é uma função
+console.log(typeof Object)
+
+//criar uma class é um artificio de sintaxe de, internamente, construir uma função
+class Produto {}
+console.log(typeof Produto)
+
